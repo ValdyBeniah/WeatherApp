998ca3d16a4e14e4c99ccb375363fb9448c7fbdb
ValdyBeniah <valdybeniah12@gmail.com>
2024-06-25 21:58:57 +0700

    first commit

---
+++ weather app vuejs/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+import SearchInput from './components/icons/SearchInput.vue';
+import WeatherCard from './components/icons/WeatherCard.vue';
+
+const places = ref([])
+
+const addPlace = (data) => {
+  places.value.push(data)
+}
+
+const deletePlace = (name) => {
+  console.log(name)
+}
+</script>
+<template>
+  <main>
+    <!-- Date -->
+    <div class="text-center mb-6">
+      {{ 
+        new Date().toLocaleDateString('en-us', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+      }}
+
+      <!--Search-->
+      <div>
+        <SearchInput @place-data="addPlace"/>
+      </div>
+      <!-- Card -->
+       <div class="grid grid-cols-2 gap-4">
+        <div v-for="(place, idx) in places" :key="idx">
+          <WeatherCard :place="place" @delete-place="deletePlace"/>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
